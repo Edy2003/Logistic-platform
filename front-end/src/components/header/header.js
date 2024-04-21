b7410d5817d0b1143e4efc6802980c67c01c3fc9
Edy2003 <86661303+Edy2003@users.noreply.github.com>
2024-04-21 23:03:20 +0300

    Header style

--- front-end/src/components/header/header.js
+++ front-end/src/components/header/header.js
@@ -2,26 +2,28 @@ import React from "react";
 import {Link, useNavigate} from "react-router-dom";
 import {doSignOut} from "../../Firebase/auth";
 import {useAuth} from "../../contexts/authContext";
+import '../../styles/header.css'
+import logo from '../../logout.png'
 
 function Header (){
     const navigate = useNavigate();
     const {userLoggedIn} = useAuth();
 
     return (
-        <>
+        <div className='header'>
             {userLoggedIn ?
-                <>
-                    <button onClick={() => doSignOut().then(() => {
+                <div className='header-links-container-logout'>
+                    <a className='header-link' onClick={() => doSignOut().then(() => {
                         navigate('/')
-                    })}>Sign out
-                    </button>
-                </>
-            :
-                <>
-                    <Link to={'/'}>Увійти</Link>
-                    <Link to={'signup'}> Зареєструватись</Link>
-                </>}
-        </>
+                    })}><img src={logo} alt='logo'/></a>
+
+                </div>
+                :
+                <div className='header-links-container'>
+                    <Link to={'/'} className='header-link'>Увійти</Link>
+                    <Link to={'signup'} className='header-link'> Зареєструватись</Link>
+                </div>}
+        </div>
     )
 
 }
